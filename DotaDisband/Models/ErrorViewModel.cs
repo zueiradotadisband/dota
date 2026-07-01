83bb8d3ea3108974dc2854033e77a9b11c4d6f45
zueiradotadisband <zueiradotadisband@gmail.com>
2026-06-30 22:42:37 -0300

    Delete DotaDisband directory

--- DotaDisband/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DotaDisband.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
